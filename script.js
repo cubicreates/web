0d302c997d487a0fd74f28ef4453662f7f0d4f73
Adit Ghosh <blubrdpirts@gmail.com>
2025-03-25 10:19:42 +0530

    v0.1

--- script.js
+++ script.js
@@ -1,1 +1,80 @@
 // Script file
+const board = document.getElementById('board');
+const statusText = document.getElementById('statusText');
+
+let currentPlayer = 'X';
+let cells = ['', '', '', '', '', '', '', '', ''];
+let active = true;
+
+let createBoard = () => {
+    board.innerHTML ="";
+    cells.forEach((cell, index) => {
+        const cellElement = document.createElement('div');
+        cellElement.classList.add('cell');
+        cellElement.innerHTML = cell;
+        cellElement.setAttribute('data-index', index);
+        cellElement.innerText = cell;
+        cellElement.addEventListener('click', handleMove);
+        board.appendChild(cellElement);
+    });
+};
+
+function handleMove(event){
+    const index = event.target.getAttribute('data-index');
+    if(cells[index] === '' && active){
+        cells[index] = currentPlayer;
+        event.target.innerText = currentPlayer;
+        checkWinner();
+        currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+        statusText.innerText = active?`Player ${currentPlayer}'s turn`:statusText.innerText;
+    }
+}
+
+function checkWinner(){
+    const winningConditions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
+    for (const combi of winCobmo){
+        const [a, b, c] = combi;
+        if(cells[a] && cells[a] === cells[b] && cells[a] === cells[c]){
+            active = false;
+            statusText.innerText = `Player ${currentPlayer} has won!`;
+            return;
+        }
+    };
+
+    let winner = false;
+    winningConditions.forEach((winningCombo) => {
+        const a = winningCombo[0];
+        const b = winningCombo[1];
+        const c = winningCombo[2];
+
+        if(cells[a] && cells[a] === cells[b] && cells[a] === cells[c]){
+            winner = true;
+            active = false;
+            statusText.innerText = `Player ${currentPlayer} has won!`;
+        }
+    });
+
+    if(!cells.includes('') && !winner){
+        active = false;
+        statusText.innerText = 'It\'s a tie!';
+    }
+
+}
+
+function resetGame(){
+    currentPlayer = 'X';
+    cells = ['', '', '', '', '', '', '', '', ''];
+    active = true;
+    statusText.innerText = `Player ${currentPlayer}'s turn`;
+    createBoard();
+}
+createBoard();
